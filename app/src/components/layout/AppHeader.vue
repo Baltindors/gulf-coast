73617051cce5c6d4e8e2e2b2c9545cc3cf02d130
Baltindors <leosan3.141@gmail.com>
2026-04-30 23:04:02 -0400

    changed logos

--- app/src/components/layout/AppHeader.vue
+++ app/src/components/layout/AppHeader.vue
@@ -16,15 +16,13 @@ const links = [
 
 <template>
   <header class="absolute top-0 left-0 right-0 z-50 flex items-center justify-between px-16 py-7">
-    <router-link to="/" class="flex flex-col items-center text-center">
-      <div class="font-serif italic text-[38px] font-medium text-gold leading-none tracking-[0.02em] relative px-1.5 flex items-center">
-        <span class="w-3.5 h-px bg-gold absolute -left-3"></span>
-        G C
-        <span class="w-3.5 h-px bg-gold absolute -right-3"></span>
-      </div>
-      <div class="font-serif text-[14px] font-medium text-navy tracking-[0.32em] mt-1">IV CONCIERGE</div>
-      <div class="font-sans text-[7.5px] tracking-[0.32em] text-slate mt-1.5 uppercase">Premium Mobile Wellness</div>
-    </router-link>
+  <router-link to="/" class="flex flex-col items-center text-center">
+    <img 
+      src="/images/logo_v2.png" 
+      alt="Gulf Coast IV Concierge Logo" 
+      class="h-[120px] md:h-[130px] w-auto object-contain transition-all duration-300"
+    />
+  </router-link>
 
 <nav class="hidden md:flex items-center gap-10">
   <router-link 
