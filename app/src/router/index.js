94d02d220d86c58c681b0b19648f27235f09dd46
Baltindors <leosan3.141@gmail.com>
2026-05-07 23:08:45 -0400

    added metadata tags

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -7,44 +7,52 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: { title: 'Mobile IV Therapy in Naples & SWFL | Gulf Coast IV Concierge' }
     },
     {
       path: '/packages',
       name: 'packages',
-      component: () => import('../views/PackagesView.vue')
+      component: () => import('../views/PackagesView.vue'),
+      meta: { title: 'IV Therapy Packages in Naples, FL | Gulf Coast IV Concierge' }
     },
     {
       path: '/how-it-works',
       name: 'how-it-works',
-      component: () => import('../views/HowItWorksView.vue')
+      component: () => import('../views/HowItWorksView.vue'),
+      meta: { title: 'How IV Therapy Works | Gulf Coast IV Concierge' }
     },
     {
       path: '/locations',
       name: 'locations',
-      component: () => import('../views/LocationsView.vue')
+      component: () => import('../views/LocationsView.vue'),
+      meta: { title: 'Mobile IV Therapy Service Areas | Naples & SWFL' }
     },
     {
       path: '/faqs',
       name: 'faqs',
-      component: () => import('../views/FaqsView.vue')
+      component: () => import('../views/FaqsView.vue'),
+      meta: { title: 'Frequently Asked Questions | Gulf Coast IV Concierge' }
     },
     {
       path: '/contact',
       name: 'contact',
-      component: () => import('../views/ContactView.vue')
+      component: () => import('../views/ContactView.vue'),
+      meta: { title: 'Contact Us | Gulf Coast IV Concierge' }
     },
     {
       path: '/privacy',
       name: 'privacy',
       component: () => import('../views/LegalView.vue'),
-      props: { type: 'privacy' }
+      props: { type: 'privacy' },
+      meta: { title: 'Privacy Policy | Gulf Coast IV Concierge' }
     },
     {
       path: '/terms',
       name: 'terms',
       component: () => import('../views/LegalView.vue'),
-      props: { type: 'terms' }
+      props: { type: 'terms' },
+      meta: { title: 'Terms & Conditions | Gulf Coast IV Concierge' }
     },
     {
       path: '/admin',
@@ -59,6 +67,10 @@ const router = createRouter({
       return { top: 0 }
     }
   }
+})
+
+router.afterEach((to) => {
+  document.title = to.meta.title || 'Gulf Coast IV Concierge'
 })
 
 export default router
